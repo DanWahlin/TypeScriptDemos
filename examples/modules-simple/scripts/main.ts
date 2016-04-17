396588a83fddf42713c697dc2be12475bf522709
Dan Wahlin <dwahlin@xmlforasp.net>
2016-04-16 20:52:18 -0700

    number to string fix

--- examples/modules-simple/scripts/main.ts
+++ examples/modules-simple/scripts/main.ts
@@ -3,4 +3,4 @@ import { BankingAccount } from './bankingAccount';
 const ba = new BankingAccount('Checking');
 ba.deposit(100);
 
-document.getElementById('balance').innerHTML = ba.balance;
+document.getElementById('balance').innerHTML = ba.balance.toString();
